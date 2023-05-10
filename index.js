a595ae39aaa5ab5749ad6bdb11d02228a50ef64a
CrystalSnake <22963880+CrystalSnake@users.noreply.github.com>
2023-05-10 21:00:49 +0400

    Refactor and add  function that return min step

--- index.js
+++ index.js
@@ -1,38 +1,50 @@
-function createBoard(size = 8) {
-  const chessBoard = [];
-  for (let i = 0; i < size; i += 1) {
-    for (let j = 0; j < size; j += 1) {
-      chessBoard.push({ x: i, y: j });
+class Cell {
+  constructor(x, y, distance = 0, route = []) {
+    this.x = x;
+    this.y = y;
+    this.distance = distance;
+    this.route = route;
+  }
+}
+
+function isOnBoard(x, y, size = 8) {
+  if (x > 0 && x < size && y > 0 && y < size) return true;
+  return false;
+}
+
+function knightMoves(start, dest, size = 8) {
+  const dx = [-2, -1, 1, 2, -2, -1, 1, 2];
+  const dy = [-1, -2, -2, -1, 1, 2, 2, 1];
+
+  const queue = [];
+  queue.push(new Cell(start[0], start[1], 0));
+
+  const visited = new Array(size);
+  for (let i = 0; i < size; i++) {
+    visited[i] = new Array(size);
+    for (let j = 0; j < size; j++) {
+      visited[i][j] = false;
     }
   }
-  return chessBoard;
-}
-const board = createBoard();
-console.log(board);
+  visited[start[0]][start[1]] = true;
 
-function knightMoves(startX, startY, destX, destY, size = 8) {
-  const moves = [
-    { x: -2, y: -1 },
-    { x: -1, y: -2 },
-    { x: 1, y: -2 },
-    { x: 2, y: -1 },
-    { x: -2, y: 1 },
-    { x: -1, y: 2 },
-    { x: 1, y: 2 },
-    { x: 2, y: 1 },
-  ];
-  const queue = [];
-  queue.push([{ x: startX, y: startY }]);
-  console.log('Queue:');
-  for (let i of queue) {
-    console.log(i);
+  let current;
+  let x, y;
+
+  while (queue.length != 0) {
+    current = queue.shift();
+    console.log(current);
+    if (current.x == dest[0] && current.y == dest[1]) return current.distance;
+    for (let i = 0; i < 8; i++) {
+      x = current.x + dx[i];
+      y = current.y + dy[i];
+      if (isOnBoard(x, y, size) && !visited[x][y]) {
+        visited[x][y] = true;
+        queue.push(new Cell(x, y, current.distance + 1));
+      }
+    }
   }
-  const visited = [];
-  visited.push([{ x: startX, y: startY }]);
-  console.log('Visited:');
-  for (let v of visited) {
-    console.log(v);
-  }
+  return current.distance;
 }
 
-knightMoves(0, 0, 1, 2, (size = 8));
+console.log(knightMoves([0, 0], [5, 6], (size = 8)));
